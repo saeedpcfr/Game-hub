17b5e30f65ec3773a43ba45d86131cbfc05b0d55
saeedpcfr <saeed.anwar@piecyfer.com>
2023-08-02 13:19:01 +0500

    Serach bar Added

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -9,7 +9,7 @@ import { HStack, Switch, Text ,useColorMode} from '@chakra-ui/react'
     return (
       <HStack>
         <Switch colorScheme='green' isChecked={colorMode==='dark'} onChange={toggleColorMode} />
-        <Text>Dark Mode</Text>
+        <Text whiteSpace='nowrap'>Dark Mode</Text>
 
        
       </HStack>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,13 +2,15 @@ import { HStack, Image, Text } from '@chakra-ui/react'
 
 import logo from '../assets/pnghut_chess-rook-download-tree.png'
 import ColorModeSwitch from './ColorModeSwitch'
+import SearchBox from './SearchBox'
 
  const Navbar=()=>  {
  
     return (
       <>
-      <HStack padding={'10px'} justifyContent={'space-between'}>
+      <HStack padding={'10px'} >
         <Image src={logo} boxSize='55px'></Image>
+        <SearchBox/>
         <ColorModeSwitch/>
       </HStack>
       </>
