35a0ff35c295584a82eaea3abbe5b9edb5fd47cf
saeedpcfr <saeed.anwar@piecyfer.com>
2023-08-02 15:14:22 +0500

    Resolve issues in sidebar

--- src/components/GnereLits.tsx
+++ src/components/GnereLits.tsx
@@ -1,4 +1,4 @@
-import { Button, HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
+import { Button, HStack, Heading, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import useGeners from "../Hooks/useGnere";
 import getCroppedImageurl from "../Services/imageurl";
 import { selectedGenre } from "../Interfaces/Reuseable";
@@ -15,12 +15,13 @@ const GnereLits = ({onSelectgenre, selectedGenre}:selectedGenre) => {
     }
   return (
   <>
+  <Heading fontSize='2xl' marginBottom={3} >Genres</Heading>
   <List>
     {data.map(gener=><ListItem paddingY='5px' key={gener.id}>
       <HStack>
         
-        <Image boxSize='30px' borderRadius={8} src={getCroppedImageurl(gener.image_background)}></Image>
-       <Button fontWeight={gener.id===selectedGenre?.id ? 'bold':'normal'} onClick={()=>onSelectgenre(gener)} variant={"ghost"} fontSize={"lg"}>{gener.name}</Button>
+        <Image objectFit='cover' boxSize='30px' borderRadius={8} src={getCroppedImageurl(gener.image_background)}></Image>
+       <Button textAlign='left' whiteSpace='normal' fontWeight={gener.id===selectedGenre?.id ? 'bold':'normal'} onClick={()=>onSelectgenre(gener)} variant={"ghost"} fontSize={"lg"}>{gener.name}</Button>
       </HStack>
     </ListItem>)}
   </List>
